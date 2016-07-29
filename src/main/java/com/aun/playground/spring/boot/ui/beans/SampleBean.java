99823642511cc15b2e04cbf89e987d18efeee010
sebastian <sebastian@kobol>
2016-07-29 22:04:22 +0200

    travis ci setup and initial build

--- src/main/java/com/aun/playground/spring/boot/ui/beans/SampleBean.java
+++ src/main/java/com/aun/playground/spring/boot/ui/beans/SampleBean.java
@@ -3,6 +3,11 @@ package com.aun.playground.spring.boot.ui.beans;
 import java.util.Random;
 import org.springframework.stereotype.Component;
 
+/**
+ * Sample bean to show some data.
+ * 
+ * @author sebastian
+ */
 @Component
 public class SampleBean {
 
