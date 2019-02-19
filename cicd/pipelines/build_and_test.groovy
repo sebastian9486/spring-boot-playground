390d342c61177c8807f5ecdc9871fef2c670b5bf
Sebastian <sommerfeld.sebastian@gmail.com>
2019-02-19 23:02:14 +0100

    jenkins build pipeline

---
+++ cicd/pipelines/build_and_test.groovy
@@ -0,0 +1,15 @@
+def project = 'sebastian9486/spring-boot-playground'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            maven("clean verify -Dproject.name=${project}/${branchName}")
+        }
+    }
+}
